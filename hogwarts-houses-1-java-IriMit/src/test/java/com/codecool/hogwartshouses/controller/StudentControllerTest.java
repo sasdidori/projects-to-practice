bc67abc187a7befb5e593c3534e80fd16e813b53
irimit <irina.mitina.ra@gmail.com>
2024-02-18 14:36:50 +0100

    added to WebMvcTest for StudentControllersTest testmethod getAll and modified PostWhenValidInput_thenReturn201() test with Mockito.verify()

--- hogwarts-houses-1-java-IriMit/src/test/java/com/codecool/hogwartshouses/controller/StudentControllerTest.java
+++ hogwarts-houses-1-java-IriMit/src/test/java/com/codecool/hogwartshouses/controller/StudentControllerTest.java
@@ -5,12 +5,14 @@ import com.codecool.hogwartshouses.service.StudentService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -20,25 +22,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = StudentController.class)
 class StudentControllerTest {
+
+    @MockBean
+    private StudentService studentService;
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
-    private StudentService studentService;
-
+   String url = "/students";
     @Test
-    void WhenValidInput_thenReturn201() throws Exception{
+    void getAll() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get(url).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        Mockito.verify(studentService).getAllStudents();
+
+    }
+    @Test
+    void PostWhenValidInput_thenReturn201() throws Exception{
         Student student = new Student("", "s", "d", "1");
 
         String requestBody = objectMapper.writeValueAsString(student);
 
 
-        mockMvc.perform(post("/students")
+        mockMvc.perform(post(url)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestBody)).andExpect(status().isCreated());
+
+        Mockito.verify(studentService).createStudent(student);
 
     }
 
